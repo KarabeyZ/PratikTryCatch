c9008ec1e119936cbf3a43a613f788628274c284
Zekeriya <Zekeriya@ZEKERIYA>
2024-08-11 13:55:11 +0300

    Add project files.

---
+++ PratikTryCatch/Program.cs
@@ -0,0 +1,15 @@
+﻿try
+{
+    Console.WriteLine("Bir sayi girin: ");
+    int userInput = Convert.ToInt32(Console.ReadLine());
+    int square = userInput * userInput;
+    Console.WriteLine("Girdiğiniz sayi: " + square);
+}
+catch (Exception)
+{
+    Console.WriteLine("Hatali giris yaptiniz, lütfen sayi giriniz.");
+}
+finally
+{
+    Console.WriteLine("İslem tamamlandi");
+}
